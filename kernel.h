cc023a0f813718bc169626c0fb1e479ab73395ff
austinatividad <austin_natividad@dlsu.edu.ph>
2024-03-29 22:56:31 +0800

    Updated files

--- kernel.h
+++ kernel.h
@@ -1,9 +1,11 @@
 double dot_product(unsigned int n, double VectorA[], double VectorB[]) {
-    double sdot = 0.0f;
-    for (unsigned int i = 0; i <= n; i++)
+    long double sdot = 0.0f;
+    unsigned int i;
+
+    for (i = 0; i < n; i++)
     {
         sdot += VectorA[i] * VectorB[i];
     }
-    
+
     return sdot;
 }
--- main.c
+++ main.c
@@ -1,40 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <string.h>
 #include <float.h>
 #include "kernel.h"
 
-extern double nasm_dot_product(int, double *, double *);
-extern void* nasm_read_p(void *);
+//CHANGE THIS VALUE TO ADJUST VECTOR SIZE
+//for reference:
+//2^20 = 1048576
+//2^24 = 16777216
+//2^30 = 1073741824
+#define V_SIZE 20
+#define N_LOOPS 1
 
-int main() {
+extern double nasm_dot_product(int, double*, double*);
+extern void* nasm_read_p(void*);
 
-    unsigned int n = 3;
+void randomVectors(long double VectorA[], double VectorB[]) {
+    int i;
+    for (i = 0; i < V_SIZE; i++) {
 
-    double VectorA[n];
-    double VectorB[n];
+        VectorA[i] = 1.0;
+        VectorB[i] = 1.0;
+    }
+    return;
+}
 
-    memset(VectorA, 1, n*sizeof(double));
-    memset(VectorB, 1, n*sizeof(double));
 
-    VectorA[0] = 1.0;
-    VectorA[1] = 2.0;
-    VectorA[2] = 3.0;
 
-    VectorB[0] = 4.0;
-    VectorB[1] = 5.0;
-    VectorB[2] = 6.0;
+int main(int number) {
+    double* VectorA = (double*)malloc(V_SIZE * sizeof(double));
+    double* VectorB = (double*)malloc(V_SIZE * sizeof(double));
 
+    /*Used to measure time between functions
+    Measuring time between functions from:
+    https://www.geeksforgeeks.org/how-to-measure-time-taken-by-a-program-in-c/
+    */
+    clock_t start = -1;
+    clock_t end = -1;
+
+
+    randomVectors(VectorA, VectorB);
+
+    
     printf("%p\n", &VectorA);
     printf("%p\n", &VectorB);
 
-    printf("C\t: %.2f\n", dot_product(3, VectorA, VectorB));
+    double C_time = 0.0;
+    double ASM_time = 0.0;
+
+    int count_c = 0;
+
+    while (count_c < N_LOOPS) {
+        //start timer
+        start = clock();
+        printf("C Run #%d \t: %.2f \n", count_c + 1, dot_product(V_SIZE, VectorA, VectorB));
+        //end timer
+        end = clock();
+        printf("Time\t\t: %.10lf\n", ((double)(end - start)) / CLOCKS_PER_SEC);
+
+        C_time = C_time + ((double)(end - start)) / CLOCKS_PER_SEC;
+
+        count_c = count_c + 1;
+    }
 
     printf("%p\n", nasm_read_p(&VectorA));
     printf("%p\n", nasm_read_p(&VectorB));
 
-    printf("NASM\t: %.2f\n", nasm_dot_product(3, VectorA, VectorB));
 
-    getchar();
+
+    int count_asm = 0;
+
+    while (count_asm < N_LOOPS) {
+        //start timer
+        start = clock();
+        printf("ASM Run #%d \t: %.2f \n", count_asm + 1, nasm_dot_product(V_SIZE, VectorA, VectorB));
+        //end timer
+        end = clock();
+        printf("Time\t\t: %.10lf\n", ((double)(end - start)) / CLOCKS_PER_SEC);
+
+        ASM_time = ASM_time + ((double)(end - start)) / CLOCKS_PER_SEC;
+
+        count_asm = count_asm + 1;
+    }
+
+    printf("Average Time (C):\t%.10lf seconds\n", C_time / N_LOOPS);
+    printf("Average Time (NASM):\t%.10lf seconds\n", ASM_time / N_LOOPS);
 
     return 0;
 }
+
